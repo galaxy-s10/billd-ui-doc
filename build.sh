54b7614f767ce5f0a8c1d5e07a771df8662df1e7
shuisheng <2274751790@qq.com>
2022-09-19 23:02:48 +0800

    25235

--- build.sh
+++ build.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 ###
 # Author: shuisheng
+# Date: 2022-04-26 01:54:48
+# Description: https://github.com/galaxy-s10/sh/blob/master/build.sh
 # Email: 2274751790@qq.com
+# FilePath: /jenkins/billd-ui-doc/build.sh
 # Github: https://github.com/galaxy-s10
-# Date: 2022-01-03 16:12:54
-# LastEditTime: 2022-01-16 20:00:40
-# Description:
+# LastEditTime: 2022-09-19 23:00:14
+# LastEditors: shuisheng
 ###
 
-# 约定$1为任务名, $2为环境, $3为Jenkins工作区
-JOBNAME=$1 # 注意: JOBNAME=$1,这个等号左右不能有空格！
-ENV=$2
-WORKSPACE=$3
-PUBLICDIR=/node
+# 该build.sh文件会在Jenkins构建完成后被执行
+# 注意:JOBNAME=$1,这个等号左右不能有空格！
+JOBNAME=$1      #约定$1为任务名
+ENV=$2          #约定$2为环境
+WORKSPACE=$3    #约定$3为Jenkins工作区
+PORT=$4         #约定$4为端口号
+TAG=$5          #约定$5为git标签
+PUBLICDIR=/node #约定公共目录为/node
 
 echo 删除node_modules:
 rm -rf node_modules
--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,13 +1,13 @@
-// import Billd from "billd-ui";
-import "billd-ui/dist/billd.css";
+import Billd from 'billd-ui';
+import 'billd-ui/dist/billd.css';
 
 export default ({ Vue, options, router }) => {
-  // Vue.use(Billd);
-  Vue.mixin({
-    mounted() {
-      import("billd-ui").then(function(m) {
-        Vue.use(m.default);
-      });
-    },
-  });
+  Vue.use(Billd);
+  // Vue.mixin({
+  //   mounted() {
+  //     import('billd-ui').then(function(m) {
+  //       Vue.use(m.default);
+  //     });
+  //   },
+  // });
 };
